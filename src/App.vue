39840ae641973cdf3c9b16c7fa10810bd57a0969
LuoJinAn <1017386624@qq.com>
2019-10-14 19:16:40 +0800

    引入重置样式reset.scss

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,17 @@
 export default {
   name: 'App'
 }
+// 利用rem根据最外层根元素的font-size来设置单位的原理
+// 让根元素的font-size自适应的话，rem的单位就是自适应的了
+document.addEventListener('DOMContentLoaded',()=>{
+  const html = document.querySelector('html')
+  let fontSize = window.innerWidth/10
+  fontSize = fontSize>36?36:fontSize  // 设置1rem最大上限为36px
+  html.style.fontSize = `${fontSize}px`
+})
+
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./scss/style.scss";
 </style>
