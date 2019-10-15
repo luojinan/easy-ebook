230685948554aaeec445de90e2929cc4532fbfc9
LuoJinAn <1017386624@qq.com>
2019-10-15 21:11:24 +0800

    页面顶部样式完成，待事件

--- src/views/ebook.vue
+++ src/views/ebook.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="ebook">
+    <!-- 页面顶部部分 -->
+    <div class="ebook-header">
+      <div class="ebook-header_left"><span class="icon-back icon"></span></div>
+      <div class="ebook-header_right">
+        <div class="ebook-header_right--icon"><span class="icon-cart icon"></span></div>
+        <div class="ebook-header_right--icon"><span class="icon-person icon"></span></div>
+        <div class="ebook-header_right--icon"><span class="icon-more icon"></span></div>
+      </div>
+    </div>
+    <!-- 阅读器主体部分 -->
     <div class="ebook-wrapper">
       <div id="book-reader"></div>
       <div class="ebook-mask">
@@ -54,6 +64,34 @@ export default {
 @import '../scss/global';
 .ebook{
   position: relative;
+  .ebook-header{
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: px2rem(48);
+    background-color: #fff;
+    z-index: 101;
+    box-shadow: 0 px2rem(8) px2rem(8) rgba(0, 0, 0, .15);
+    .ebook-header_left{
+      flex: 0 0 px2rem(40);
+      @include center;
+      .icon-back{
+        font-size: px2rem(18)
+      }
+    }
+    .ebook-header_right{
+      flex: 1;
+      display: flex;
+      justify-content: flex-end;  // 只写end是无效的
+      .ebook-header_right--icon{
+        flex: 0 0 px2rem(40);
+        @include center;
+      }
+    }
+  }
+
   .ebook-wrapper{
     .ebook-mask{
       display: flex;
